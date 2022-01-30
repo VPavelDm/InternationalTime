0d7be429dcca1648b5f18566dab8e8e4e01203b2
Pavel Vaitsikhouski <pavel.vaitsikhouski@adevinta.com>
2022-01-30 17:43:46 +0300

    Initial Commit

---
+++ InternationalTime/InternationalTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternationalTimeApp.swift
+//  InternationalTime
+//
+//  Created by Pavel Vaitsikhouski on 30.01.22.
+//
+
+import SwiftUI
+
+@main
+struct InternationalTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
